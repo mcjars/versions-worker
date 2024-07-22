25304f3496d05ace6fe74aae3fd37f25957ea243
0x7d8 <rjansengd@gmail.com>
2024-07-22 16:04:21 +0200

    add back size property on /api/v1/stats

--- src/api/v1/stats.ts
+++ src/api/v1/stats.ts
@@ -1,10 +1,10 @@
-import { count, countDistinct } from "drizzle-orm"
+import { count, countDistinct, sql } from "drizzle-orm"
 import { GlobalRouter } from "../.."
 import { time } from "@rjweb/utils"
 
 export default function(router: GlobalRouter) {
 	router.get('/api/v1/stats', async({ req }) => {
-		const [ data, requests ] = await req.cache.use('stats::all', () => Promise.all([
+		const [ data, requests, database ] = await req.cache.use('stats::all', () => Promise.all([
 				req.database.select({
 					builds: countDistinct(req.database.schema.buildHashes.buildId),
 					hashes: count()
@@ -15,7 +15,8 @@ export default function(router: GlobalRouter) {
 					requests: count()
 				})
 					.from(req.database.schema.requests)
-					.get()
+					.get(),
+				req.database.run(sql`SELECT 1`)
 			]),
 			time(30).m()
 		)
@@ -25,7 +26,10 @@ export default function(router: GlobalRouter) {
 			stats: {
 				builds: data?.builds ?? 0,
 				hashes: data?.hashes ?? 0,
-				requests: requests?.requests ?? 0
+				requests: requests?.requests ?? 0,
+				size: {
+					database: database?.meta.size_after ?? 0
+				}
 			}
 		})
 	})
