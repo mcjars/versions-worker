110b0e6755487706dcef489898dec10b7e69cd96
Robert Jansen <rjansengd@gmail.com>
2024-10-16 16:33:25 +0000

    add /api/v2/shares apis

--- src/api/v2/index.ts
+++ src/api/v2/index.ts
@@ -5,6 +5,7 @@ import configRouter from "./config"
 import statsRouter from "./stats"
 import requestsRouter from "./requests"
 import buildsRouter from "./builds"
+import sharesRouter from "./shares"
 
 export default function(router: GlobalRouter) {
 	buildRouter(router)
@@ -12,4 +13,5 @@ export default function(router: GlobalRouter) {
 	statsRouter(router)
 	requestsRouter(router)
 	buildsRouter(router)
+	sharesRouter(router)
 }
---
+++ src/api/v2/shares.ts
@@ -0,0 +1,94 @@
+import { and, count, countDistinct, desc, isNotNull, like, notLike, sql } from "drizzle-orm"
+import { GlobalRouter } from "../.."
+import { object, time } from "@rjweb/utils"
+import { ServerType, types } from "../../schema"
+
+export default function(router: GlobalRouter) {
+	router.get('/api/v2/shares/versions', async({ req }) => {
+		const versions = await req.cache.use('shares::versions', () => req.database.select({
+				version: sql<string>`json_extract(${req.database.schema.requests.data}, '$.build.versionId')`.as('version'),
+				total: count().as('total'),
+				uniqueIps: countDistinct(req.database.schema.requests.ip)
+			})
+				.from(req.database.schema.requests)
+				.where(and(
+					isNotNull(req.database.schema.requests.data),
+					notLike(req.database.schema.requests.path, `%tracking=nostats%`),
+					like(req.database.schema.requests.data, `%lookup%`),
+					isNotNull(sql`version`)
+				))
+				.groupBy(sql`version`)
+				.orderBy(desc(sql`total`))
+				.all(),
+			time(30).m()
+		)
+
+		return Response.json({
+			success: true,
+			versions: Object.fromEntries(versions.map((version) => [
+				version.version,
+				object.pick(version, ['total', 'uniqueIps'])
+			]))
+		})
+	})
+
+	router.get('/api/v2/shares/versions/:type', async({ req }) => {
+		const type = req.params.type.toUpperCase() as ServerType
+		if (!types.includes(type)) return Response.json({ success: false, errors: ['Invalid type'] }, { status: 400 })
+
+		const versions = await req.cache.use(`shares::versions::${type}`, () => req.database.select({
+				version: sql<string>`json_extract(${req.database.schema.requests.data}, '$.build.versionId')`.as('version'),
+				total: count().as('total'),
+				uniqueIps: countDistinct(req.database.schema.requests.ip)
+			})
+				.from(req.database.schema.requests)
+				.where(and(
+					isNotNull(req.database.schema.requests.data),
+					notLike(req.database.schema.requests.path, `%tracking=nostats%`),
+					like(req.database.schema.requests.data, `%lookup%`),
+					like(req.database.schema.requests.data, `%${type}%`),
+					isNotNull(sql`version`)
+				))
+				.groupBy(sql`version`)
+				.orderBy(desc(sql`total`))
+				.all(),
+			time(30).m()
+		)
+
+		return Response.json({
+			success: true,
+			versions: Object.fromEntries(versions.map((version) => [
+				version.version,
+				object.pick(version, ['total', 'uniqueIps'])
+			]))
+		})
+	})
+
+	router.get('/api/v2/shares/types', async({ req }) => {
+		const types = await req.cache.use('shares::types', () => req.database.select({
+				type: sql<string>`json_extract(${req.database.schema.requests.data}, '$.build.type')`.as('version'),
+				total: count().as('total'),
+				uniqueIps: countDistinct(req.database.schema.requests.ip)
+			})
+				.from(req.database.schema.requests)
+				.where(and(
+					isNotNull(req.database.schema.requests.data),
+					notLike(req.database.schema.requests.path, `%tracking=nostats%`),
+					like(req.database.schema.requests.data, `%lookup%`),
+					isNotNull(sql`version`)
+				))
+				.groupBy(sql`version`)
+				.orderBy(desc(sql`total`))
+				.all(),
+			time(30).m()
+		)
+
+		return Response.json({
+			success: true,
+			types: Object.fromEntries(types.map((type) => [
+				type.type,
+				object.pick(type, ['total', 'uniqueIps'])
+			]))
+		})
+	})
+}
