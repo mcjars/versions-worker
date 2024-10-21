537a9c7c1f3dbd71aad7ad68e3fdee8a5d31fe26
Robert Jansen <rjansengd@gmail.com>
2024-10-21 12:26:12 +0000

    drastically optimize share apis

--- src/api/v2/shares.ts
+++ src/api/v2/shares.ts
@@ -6,18 +6,23 @@ import { ServerType, types } from "../../schema"
 export default function(router: GlobalRouter) {
 	router.get('/api/v2/shares/versions', async({ req }) => {
 		const versions = await req.cache.use('shares::versions', () => req.database.select({
-				version: sql<string>`json_extract(${req.database.schema.requests.data}, '$.build.versionId')`.as('version'),
+				version: sql<string>`x.version`.as('version'),
 				total: count().as('total'),
 				uniqueIps: countDistinct(req.database.schema.requests.ip)
 			})
-				.from(req.database.schema.requests)
-				.where(and(
-					isNotNull(req.database.schema.requests.data),
-					notLike(req.database.schema.requests.path, `%tracking=nostats%`),
-					like(req.database.schema.requests.data, `%lookup%`),
-					isNotNull(sql`version`)
-				))
-				.groupBy(sql`version`)
+				.from(req.database.select({
+					version: sql<string>`json_extract(${req.database.schema.requests.data}, '$.build.versionId')`.as('version'),
+					ip: req.database.schema.requests.ip
+				})
+					.from(req.database.schema.requests)
+					.where(and(
+						isNotNull(req.database.schema.requests.data),
+						notLike(req.database.schema.requests.path, `%tracking=nostats%`),
+						like(req.database.schema.requests.data, `%lookup%`)
+					))
+					.as('x'))
+				.where(isNotNull(sql`x.version`))
+				.groupBy(sql`x.version`)
 				.orderBy(desc(sql`total`))
 				.all(),
 			time(30).m()
@@ -37,19 +42,24 @@ export default function(router: GlobalRouter) {
 		if (!types.includes(type)) return Response.json({ success: false, errors: ['Invalid type'] }, { status: 400 })
 
 		const versions = await req.cache.use(`shares::versions::${type}`, () => req.database.select({
-				version: sql<string>`json_extract(${req.database.schema.requests.data}, '$.build.versionId')`.as('version'),
+				version: sql<string>`x.version`.as('version'),
 				total: count().as('total'),
 				uniqueIps: countDistinct(req.database.schema.requests.ip)
 			})
-				.from(req.database.schema.requests)
-				.where(and(
-					isNotNull(req.database.schema.requests.data),
-					notLike(req.database.schema.requests.path, `%tracking=nostats%`),
-					like(req.database.schema.requests.data, `%lookup%`),
-					like(req.database.schema.requests.data, `%${type}%`),
-					isNotNull(sql`version`)
-				))
-				.groupBy(sql`version`)
+				.from(req.database.select({
+					version: sql<string>`json_extract(${req.database.schema.requests.data}, '$.build.versionId')`.as('version'),
+					ip: req.database.schema.requests.ip
+				})
+					.from(req.database.schema.requests)
+					.where(and(
+						isNotNull(req.database.schema.requests.data),
+						notLike(req.database.schema.requests.path, `%tracking=nostats%`),
+						like(req.database.schema.requests.data, `%lookup%`),
+						like(req.database.schema.requests.data, `%${type}%`)
+					))
+					.as('x'))
+				.where(isNotNull(sql`x.version`))
+				.groupBy(sql`x.version`)
 				.orderBy(desc(sql`total`))
 				.all(),
 			time(30).m()
@@ -66,18 +76,23 @@ export default function(router: GlobalRouter) {
 
 	router.get('/api/v2/shares/types', async({ req }) => {
 		const types = await req.cache.use('shares::types', () => req.database.select({
-				type: sql<string>`json_extract(${req.database.schema.requests.data}, '$.build.type')`.as('version'),
+				type: sql<string>`x.type`.as('type'),
 				total: count().as('total'),
 				uniqueIps: countDistinct(req.database.schema.requests.ip)
 			})
-				.from(req.database.schema.requests)
-				.where(and(
-					isNotNull(req.database.schema.requests.data),
-					notLike(req.database.schema.requests.path, `%tracking=nostats%`),
-					like(req.database.schema.requests.data, `%lookup%`),
-					isNotNull(sql`version`)
-				))
-				.groupBy(sql`version`)
+				.from(req.database.select({
+					type: sql<string>`json_extract(${req.database.schema.requests.data}, '$.build.type')`.as('type'),
+					ip: req.database.schema.requests.ip
+				})
+					.from(req.database.schema.requests)
+					.where(and(
+						isNotNull(req.database.schema.requests.data),
+						notLike(req.database.schema.requests.path, `%tracking=nostats%`),
+						like(req.database.schema.requests.data, `%lookup%`)
+					))
+					.as('x'))
+				.where(isNotNull(sql`x.type`))
+				.groupBy(sql`x.type`)
 				.orderBy(desc(sql`total`))
 				.all(),
 			time(30).m()
