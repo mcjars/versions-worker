6a9f2621de76856259d0421769a1372d71dac8ff
Robert Jansen <rjansengd@gmail.com>
2024-10-27 15:18:19 +0000

    remove robots.txt

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -13,8 +13,4 @@ export default function(router: GlobalRouter) {
 	router.get('/openapi.json', () => {
 		return Response.json(openApi)
 	})
-
-	router.get('/robots.txt', () => {
-		return new Response('User-agent: *\nDisallow: /api')
-	})
 }
